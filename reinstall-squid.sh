a2f497fe3a764cf555e7f8bf041fa314b09b0c00
koichog <manqka_Cs4o@abv.bg>
2023-04-25 20:56:33 +0100

    Update reinstall-squid.sh

--- reinstall-squid.sh
+++ reinstall-squid.sh
@@ -1,42 +1,57 @@
-echo "Installing necessary packages"
-sudo apt-get remove --purge squid -y
-sudo apt-get autoremove -y
-sudo apt-get update
-sudo apt-get install -y build-essential libssl-dev openssl libnetfilter-conntrack-dev
-SQUID_VERSION=4.13
+#!/bin/bash
 
-echo "Retrieving squid m squid-cache.org"
-sudo wget http://www.squid-cache.org/Versions/v4/squid-${SQUID_VERSION}.tar.gz
-sudo tar xzf squid-${SQUID_VERSION}.tar.gz
+# Define Squid version
+SQUID_VERSION="4.13"
+
+# Uninstall existing Squid package
+sudo apt-get remove --purge squid
+
+# Install build dependencies
+sudo apt-get install -y build-essential libssl-dev
+
+# Download Squid source code
+wget http://www.squid-cache.org/Versions/v4/squid-${SQUID_VERSION}.tar.gz
+
+# Extract the source code
+tar xvf squid-${SQUID_VERSION}.tar.gz
+
+# Change to the extracted directory
 cd squid-${SQUID_VERSION}
 
-echo "Configuring Squid"
-./configure --enable-ssl --with-openssl --enable-ssl-crtd
+# Configure the Squid installation with the necessary flags
+./configure --prefix=/usr --localstatedir=/var --libexecdir=${prefix}/lib/squid --datadir=${prefix}/share/squid --sysconfdir=/etc/squid --with-default-user=pi --with-logdir=/var/log/squid --with-pidfile=/var/run/squid.pid --enable-ssl --with-openssl --enable-ssl-crtd
 
-echo "Installing it - this will take time so take a deep breath and come back later :)"
-sleep 5
-sudo make
+# Build and install Squid
+sudo make -j$(nproc)
 sudo make install
-sudo bash -c "cat > /etc/systemd/system/squid.service << EOL
+
+# Create required directories and set permissions
+sudo mkdir -p /var/log/squid /var/cache/squid
+sudo chown -R pi:pi /var/log/squid /var/cache/squid
+
+# Set up Squid to run as a service
+sudo bash -c 'cat > /etc/systemd/system/squid.service << EOL
 [Unit]
 Description=Squid Web Proxy Server
 After=network.target
 
 [Service]
 Type=forking
-ExecStart=/usr/sbin/squid -sYC
-ExecReload=/usr/sbin/squid -k reconfigure
-PIDFile=/var/run/squid.pid
+ExecStartPre=/usr/lib/squid/squid -z
+ExecStart=/usr/lib/squid/squid -sYC
+ExecReload=/bin/kill -HUP \$MAINPID
+ExecStop=/bin/kill -TERM \$MAINPID
+Restart=on-abort
+User=pi
+Group=pi
 
 [Install]
 WantedBy=multi-user.target
-EOL"
+EOL'
 
-sudo ln -s /usr/local/squid/sbin/squid /usr/sbin/squid
-sudo chown -R 777 /usr/local/squid
-sudo chmod -R 777 /usr/local/squid
-
-# Reload systemd and enable Squid service
+# Reload the systemd daemon and enable Squid to start on boot
 sudo systemctl daemon-reload
 sudo systemctl enable squid
-echo "Squid installation complete."
+
+# Start the Squid service
+sudo systemctl start squid
